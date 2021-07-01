3f35d7c10d11e84f44f4e55dca60269d5b729eb9
Stephen J. Eglen <sje30@cam.ac.uk>
2021-07-01 13:06:43 +0100

    periodic update

--- stone1996.jl
+++ stone1996.jl
@@ -16,48 +16,31 @@ wts_final = vec(readdlm("data/wts.990"))
 typeof(wts)
 varinfo()  ## show all variable info
 
+net = new_net(inputs, wts);
 
-function new_net(inputs, wts)
-    z = zeros(22)
-    z1 = zeros(22)
-    lambda_s = 2.0^ 1/32
-    lambda_l = 2.0^ 1/3200
-    U = 0.0
-    V = 0.0
-    F = 0.0
-    ztilde = 0.0
-    zbar = 0.0
-    nwts = 120
-    dUdw = zeros(nwts)
-    dVdw = zeros(nwts)
-    dzdw = zeros(nwts)
-    dzdw1 = zeros(nwts)
-    dztildedw1 = zeros(nwts)
-    dztildedw = zeros(nwts)
-    dzbardw1 = zeros(nwts)
-    dzbardw = zeros(nwts)
-    dFdw = zeros(nwts)
-    net = Net(inputs, wts, 11, 10, 1,
-              lambda_s, lambda_l,
-              U, V, F, 
-              ztilde, zbar,
-              z, z1,
-              dUdw, dVdw,
-              dzdw, dzdw1,
-              dztildedw, dztildedw1,
-              dzbardw, dzbardw1,
-              dFdw
-              )
-    net
-end
-
-net = new_net(inputs, wts)
 forward(net, 1)
 backward(net)
+remember(net)
+plot(net.dUdw)
+forward(net, 2)
+@show net.U, net.V
 
+for j=1:10, i=1:1000
+    forward(net, i)
+    backward(net)
+    remember(net)
+    if i==999
+        @show net.F, net.V, net.U
+    end
+end
+
+    
 @time allz = [forward(net, x) for x in 1:1000]
 plot(allz, xlabel="input number", ylabel="output")
 
+
+
+# Check that things are still working...
 
 netf = new_net(inputs, wts_final)
 @time allz = [forward(netf, x) for x in 1:1000]
