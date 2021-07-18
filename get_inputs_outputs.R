6389b944dbe8f029694317cb2db5270d79ef2b68
Stephen J. Eglen <sje30@cam.ac.uk>
2021-07-18 16:28:20 +0100

    try backprop

---
+++ get_inputs_outputs.R
@@ -0,0 +1,27 @@
+## Fitting a back prop network to the data.
+## [2021-07-18 Sun]
+
+
+require(nnet)
+
+left <- scan("data/image1.txt", nlines=1)
+right <- scan("data/image2.txt", nlines=1)
+z <- scan("data/shifts.txt", nlines=1)
+
+inputs <- matrix(0, 1000, 10)
+outputs <- rep(0, 1000)
+p <- 1
+
+for (i in 1:1000) {
+  inputs[i,1:5] <- left[p:(p+4)]
+  inputs[i,6:10] <- right[p:(p+4)]
+  outputs[i] <- z[p+2]
+  p <- p + 7
+}
+
+## Note the network has 121 weights rather than 120 as before -- the output unit
+## has a bias unit too.
+
+p = nnet(inputs, outputs, size=10, linout=T)
+
+plot(p$fitted)
